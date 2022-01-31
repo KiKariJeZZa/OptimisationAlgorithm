46fe62599d4ae0deb2893cd0a38a25d45dcdabfe
Jeremiah Casuga <jerrycasuga9@gmail.com>
2022-01-31 14:53:16 +1000

    Added barebone skeleton gui

--- gui.py
+++ gui.py
@@ -6,20 +6,76 @@
 # Author: Jeremiah Casuga
 # ----------------------------------------------
 
-from tkinter import Tk, Label, Button
-import header
-import energetic
+from tkinter import *
+import header as header
+import energetic as energetic
 
 class GUI:
     def __init__(self, master):
         self.master = master
         master.title("Optimisation Algorithm")
 
-        self.label = Label(master, text="Optimisation Algorithm")
-        self.label.pack()
+        self.master.label = Label(master, text="Optimisation Algorithm")
+        self.master.label.config(font = ('Helvetica bold',30))
+        self.master.label.place(relx = 0.5, rely = 0.5, anchor = 'center')
+        self.master.label.pack()
 
+    def drop_menu(self):
+        return 0
+
+    def button(self):
+        return 0
 
     
-root = Tk()
-gui = GUI(root)
-root.mainloop()
+    def new_window_energetic(self):
+        name_energetic = StringVar()
+        ROI_energetic = StringVar()
+        newWindow = Toplevel(self.master)
+        newWindow.title("Enter new energetic")
+        newWindow.geometry("300x100")
+
+        submit_btn = Button(newWindow, text = 'Okay')
+
+        name_label = Label(newWindow, text = "Name")
+        new_name = Entry(newWindow, textvariable=name_energetic)
+        ROI_label = Label(newWindow, text = "ROI")
+        new_ROI = Entry(newWindow, textvariable=ROI_energetic)
+        name_label.grid(row=0,column=0)
+        new_name.grid(row=0,column=1)
+        ROI_label.grid(row=1,column=0)
+        new_ROI.grid(row=1,column=1)
+        submit_btn.grid(row=2,column=0)
+
+if __name__ == '__main__':
+    root = Tk()
+    root.geometry("600x600")
+
+
+    gui = GUI(root)
+
+    list = {
+        "A4":2,
+        "S4":3
+    }
+    
+
+    header_database = [
+        "4x4",
+        "4x5",
+        "4x6"
+    ]
+
+    variable = StringVar(root)
+    variable.set("Select energetic")
+    energetics = OptionMenu(root, variable, *energetic.Energetics(list).get_energetics())
+    energetics.place(x=150, y = 50, anchor='nw')
+
+    variable2 = StringVar(root)
+    variable2.set("Select header size")
+    headers = OptionMenu(root, variable2, *header_database)
+    headers.place(x=450,y=50,anchor='ne')
+
+    add_energetic_button = Button(root, text = "Add energetic", command = gui.new_window_energetic)
+    add_energetic_button.place(x = 250, y=85)
+
+    root.mainloop()
